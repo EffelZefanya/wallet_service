b978a2d3ed02a50e9de183f0ca5401306ccd2dc3
Effel <99955631+EffelZefanya@users.noreply.github.com>
2026-02-18 14:43:04 +0700

    feat: erd and query

---
+++ sql/ddl.sql
@@ -0,0 +1,29 @@
+-- Created in PSQL
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE wallets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    balance DECIMAL(15, 2) DEFAULT 0.00,
+    version INT DEFAULT 0,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- For this case, only WITHDRAWAL is available
+CREATE TYPE tx_type AS ENUM ('WITHDRAWAL', 'DEPOSIT');
+-- For this case, only SUCCESS and FAILED is available. PENDING usually is set if a confirmation is needed. Like waiting for a TOP UP through a payment gateway when depositing money.
+CREATE TYPE tx_status AS ENUM ('PENDING', 'SUCCESS', 'FAILED');
+
+CREATE TABLE transactions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    wallet_id UUID REFERENCES wallets(id),
+    amount DECIMAL(15, 2) NOT NULL,
+    transaction_type tx_type NOT NULL,
+    status tx_status NOT NULL DEFAULT 'PENDING',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
